dd423ec99ca3544e1eddbcd0a84422aa6f6fa8ff
Benbrns <benbranders2@gmail.com>
2026-04-02 22:51:44 +0200

    refactor code and added allocate

--- src/App.java
+++ src/App.java
@@ -1,10 +1,16 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
         StorageDevice storage = new StorageDevice(10, 5);
         
-        System.out.println(">>Block size\n" + storage.getBlockSize());
-        System.out.println(">>Total block count\n" + storage.getBlockCount());
-        System.out.println(">>Free block count\n" + storage.getFreeBlocksCount());
-        System.out.println(">>Unavailable block count\n" + storage.getUnavailableBlocksCount());
+        System.out.println(">>Block size\n" + storage.blockSize());
+        System.out.println(">>Total block count\n" + storage.totalBlockCount());
+        System.out.println(">>Free block count\n" + storage.freeBlocksCount());
+        System.out.println(">>Unavailable block count\n" + storage.usedBlocksCount());
+
+        System.out.println(">>Allocated blocks\n" + storage.allocate(5));
+        System.out.println(">>Allocated blocks\n" + storage.allocate(3));
+        System.out.println(">>Allocated blocks\n" + storage.allocate(2));
+        System.out.println(">>Allocated blocks\n" + storage.allocate(1));
     }
 }
--- src/StorageDevice.java
+++ src/StorageDevice.java
@@ -1,49 +1,55 @@
+import java.util.ArrayList;
 
 public class StorageDevice {
 
     private int blockCount;
     private int blockSize;
-
-    // Free Blocks to use
-    private int[] freeBlocks;
-    // used blocks
-    private int[] unavailableBlocks;
+    private ArrayList<Integer> freeBlocks = new ArrayList<Integer>();
+    private ArrayList<Integer> usedBlocks = new ArrayList<Integer>();
 
     public StorageDevice(int blockCount, int blockSize) {
         this.blockCount = blockCount;
         this.blockSize = blockSize;
         this.freeBlocks = initFreeBlocks(blockCount);
-        this.unavailableBlocks = initUnavailableBlocks(blockCount);
     }
 
-    //SETTERS
-    public int[] initFreeBlocks(int blockCount) {
-        int[] freeBlocks = new int[blockCount];
-        for (int i = 0;i < blockCount;i++) {
-            freeBlocks[i] = i;
+    public ArrayList<Integer> allocate(int blockCount) {
+        
+        if (freeBlocks.size() < blockCount) {
+            throw new IllegalArgumentException("No available free space left");
+        }
+        
+        for (int i = 0; i < blockCount; i++) {
+            int block = freeBlocks.remove(0);
+            usedBlocks.add(block);
+        }
+        
+        return usedBlocks;
+    }
+
+    //INIT
+    public ArrayList<Integer> initFreeBlocks(int blockCount) {
+        for (int i = 0;i < blockCount ; i++) {
+            freeBlocks.add(i);
         }
         return freeBlocks;
     }
-
-    public int[] initUnavailableBlocks(int blockCount) {
-        return new int[blockCount];
-    }
-
+    
     // GETTERS
-    public int getBlockCount() {
+    public int totalBlockCount() {
         return this.blockCount;
     }
 
-    public int getBlockSize() {
+    public int blockSize() {
         return this.blockSize;
     }
 
-    public int getFreeBlocksCount() {
-        return this.freeBlocks.length;
+    public int freeBlocksCount() {
+        return this.freeBlocks.size();
     }
 
-    public int getUnavailableBlocksCount() {
-        return this.unavailableBlocks.length;
+    public int usedBlocksCount() {
+        return this.usedBlocks.size();
     }
 
 }
