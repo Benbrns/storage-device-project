b1bdbf898974e1a462258793586f29133e7a718e
Benbrns <benbranders2@gmail.com>
2026-04-03 19:58:48 +0200

    added allocate and free function

--- src/App.java
+++ src/App.java
@@ -1,16 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        StorageDevice storage = new StorageDevice(10, 5);
-        
-        System.out.println(">>Block size\n" + storage.blockSize());
-        System.out.println(">>Total block count\n" + storage.totalBlockCount());
-        System.out.println(">>Free block count\n" + storage.freeBlocksCount());
-        System.out.println(">>Unavailable block count\n" + storage.usedBlocksCount());
+	public static void main(String[] args) throws Exception {
+		StorageDevice storage = new StorageDevice(10, 5);
 
-        System.out.println(">>Allocated blocks\n" + storage.allocate(5));
-        System.out.println(">>Allocated blocks\n" + storage.allocate(3));
-        System.out.println(">>Allocated blocks\n" + storage.allocate(2));
-        System.out.println(">>Allocated blocks\n" + storage.allocate(1));
-    }
+		System.out.println(">>Block size\n" + storage.blockSize() + "\n");
+		System.out.println(">>Total block count\n" + storage.totalBlockCount() + "\n");
+		System.out.println(">>Free block count\n" + storage.freeBlocksCount() + "\n");
+		System.out.println(">>Unavailable block count\n" + storage.usedBlocksCount() + "\n");
+
+		System.out.println(">>Allocated blocks\n" + storage.allocate(5) + "\n");
+		System.out.println(">>Object\n" + storage.toString() + "\n");
+
+		System.out.println(">>Allocated blocks\n" + storage.allocate(3) + "\n");
+		System.out.println(">>Object\n" + storage.toString() + "\n");
+
+		System.out.println(">>Allocated blocks\n" + storage.allocate(2) + "\n");
+		System.out.println(">>Object\n" + storage.toString() + "\n");
+
+		// System.out.println(">>Allocated blocks\n" + storage.allocate(1));
+
+		List<Integer> blocksToFree1 = new ArrayList<>();
+		blocksToFree1.add(1);
+		blocksToFree1.add(2);
+		blocksToFree1.add(3);
+		System.out.println(">>Free blocks\n" + storage.free(blocksToFree1) + "\n");
+		System.out.println(">>Object\n" + storage.toString() + "\n");
+
+		List<Integer> blocksToFree2 = new ArrayList<>();
+		blocksToFree2.add(0);
+		blocksToFree2.add(4);
+		blocksToFree2.add(5);
+		blocksToFree2.add(6);
+		blocksToFree2.add(7);
+		blocksToFree2.add(8);
+		blocksToFree2.add(9);
+		System.out.println(">>Free blocks\n" + storage.free(blocksToFree2) + "\n");
+		System.out.println(">>Object\n" + storage.toString() + "\n");
+	}
 }
--- src/StorageDevice.java
+++ src/StorageDevice.java
@@ -1,55 +1,71 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class StorageDevice {
+	private int blockCount;
+	private int blockSize;
+	private List<Integer> freeBlocks = new ArrayList<Integer>();
+	private List<Integer> usedBlocks = new ArrayList<Integer>();
 
-    private int blockCount;
-    private int blockSize;
-    private ArrayList<Integer> freeBlocks = new ArrayList<Integer>();
-    private ArrayList<Integer> usedBlocks = new ArrayList<Integer>();
+	public StorageDevice(int blockCount, int blockSize) {
+		this.blockCount = blockCount;
+		this.blockSize = blockSize;
+		this.freeBlocks = initFreeBlocks(blockCount);
+	}
 
-    public StorageDevice(int blockCount, int blockSize) {
-        this.blockCount = blockCount;
-        this.blockSize = blockSize;
-        this.freeBlocks = initFreeBlocks(blockCount);
-    }
+	public List<Integer> allocate(int blockCount) {
+		if (freeBlocks.size() < blockCount) {
+			throw new IllegalArgumentException("No available free space left");
+		}
+		for (int i = 0; i < blockCount; i++) {
+			int block = freeBlocks.remove(0);
+			usedBlocks.add(block);
+		}
+		return usedBlocks;
+	}
 
-    public ArrayList<Integer> allocate(int blockCount) {
-        
-        if (freeBlocks.size() < blockCount) {
-            throw new IllegalArgumentException("No available free space left");
-        }
-        
-        for (int i = 0; i < blockCount; i++) {
-            int block = freeBlocks.remove(0);
-            usedBlocks.add(block);
-        }
-        
-        return usedBlocks;
-    }
+	public List<Integer> free(List<Integer> blocks) {
+		for (int i = 0; i < blocks.size(); i++) {
+			int block = blocks.get(i);
+			if (!usedBlocks.contains(block)) {
+				throw new IllegalArgumentException("Block is not used");
+			}
+			usedBlocks.remove(usedBlocks.indexOf(block));
+			freeBlocks.add(block);
+		}
+		freeBlocks.sort(null);
+		return freeBlocks;
+	}
 
-    //INIT
-    public ArrayList<Integer> initFreeBlocks(int blockCount) {
-        for (int i = 0;i < blockCount ; i++) {
-            freeBlocks.add(i);
-        }
-        return freeBlocks;
-    }
-    
-    // GETTERS
-    public int totalBlockCount() {
-        return this.blockCount;
-    }
+	//INIT
+	public List<Integer> initFreeBlocks(int blockCount) {
+		for (int i = 0;i < blockCount ; i++) {
+			freeBlocks.add(i);
+		}
+		return freeBlocks;
+	}
+	
+	// GETTERS
+	public int totalBlockCount() {
+		return this.blockCount;
+	}
 
-    public int blockSize() {
-        return this.blockSize;
-    }
+	public int blockSize() {
+		return this.blockSize;
+	}
 
-    public int freeBlocksCount() {
-        return this.freeBlocks.size();
-    }
+	public int freeBlocksCount() {
+		return this.freeBlocks.size();
+	}
 
-    public int usedBlocksCount() {
-        return this.usedBlocks.size();
-    }
+	public int usedBlocksCount() {
+		return this.usedBlocks.size();
+	}
+
+	@Override
+	public String toString() {
+		return "StorageDevice [blockCount=" + blockCount + ", blockSize=" + blockSize + ", freeBlocks=" + freeBlocks
+				+ ", usedBlocks=" + usedBlocks + "]";
+	}
 
 }
